85fc2f65197723b7ba121b5c75620174e122b849
Gurumi432 <83202258+Gurumi432@users.noreply.github.com>
2025-07-04 20:09:26 +0900

    build.gradle

--- src/main/java/dev/mapofdev/MapofdevApplication.java
+++ src/main/java/dev/mapofdev/MapofdevApplication.java
@@ -2,13 +2,15 @@ package dev.mapofdev; // 패키지 경로 정의 (폴더 구조와 일치)
 
 import org.springframework.boot.SpringApplication; // 스프링부트 앱 실행 도구
 import org.springframework.boot.autoconfigure.SpringBootApplication; 
-// 복합 어노테이션 임포트, 앱의 시작점(Spring Boot의 진입점) + 필요한 설정 자동 구성 + 컴포넌트 자동 스캔
-//이노테이션이란? 코드에 붙이는 부가적인 명령어 (컴파일러, 프레임워크, API 등등에 대한 선언 혹은 명령)
+// 복합 Annotaition에 대한  import(앱의 실행 + 필요한 설정 자동 구성 + 컴포넌트 자동 스캔)
+/*Annotation은 자바 코드에 @ 기호를 이용해 부착되는 metadata(데이터를 설명하는 데이터)`로,
+컴파일러, 빌드 도구, 런타임 환경 또는 프레임워크가 해당 코드에 특별한 의미나 동작을 부여하도록 하는 기법이다.
+예: @RequestParam, @Override, @GetMapping 등 */
+
 //프레임워크는 "코드를 짜는 틀"
 //API는 "코드를 통해 쓰는 기능집"
 
-@SpringBootApplication // 이 클래스 쓰겠다고 선언한거 (선언 전에 임포트 해야함)
-
+@SpringBootApplication // 스프링 부트 애플리케이션의 시작점(main class)으로 지정
 public class MapofdevApplication { //Spring Boot 앱을 실행시키는 "시동 버튼" 역할 클래스
 
 public static void main(String[] args) {                      // 자바 애플리케이션의 진입점(시작 메서드)
